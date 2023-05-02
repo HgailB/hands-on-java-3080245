4bb95df20bee80350795b2752c6c67b8f84b1289
Heather Gailey <84522235+HgailB@users.noreply.github.com>
2023-05-02 04:35:17 +0000

    Add Account Class to return account information

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -1,18 +1,22 @@
 package bank;
 
+// main is first method executed when running Java program
+// java.sql is built-in package that supports db interactions 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 public class DataSource {
 
+  // Connection represents connection to a database 
   public static Connection connect() {
     String db_file = "jdbc:sqlite:resources/bank.db";
     Connection connection = null;
     
+    // use try/catch block to handle exceptions
+    // Try-With-Resources auto-closes spec. resources after execution of structure
     try {
       connection = DriverManager.getConnection(db_file);
       System.out.println(db_file);
@@ -27,11 +31,13 @@ public class DataSource {
     String sql = "select * from customers where username = ?";
     Customer customer = null;
 
+    // Execute an SQL statement using PreparedStatement Java class
     try(Connection connection = connect();
       PreparedStatement statement = connection.prepareStatement(sql)){
 
         statement.setString(1, username);
         try(ResultSet resultSet = statement.executeQuery()){
+          // Use resultSet to return data from database query
           customer = new Customer(
             resultSet.getInt("id"),
             resultSet.getString("name"),
@@ -39,16 +45,37 @@ public class DataSource {
             resultSet.getString("password"),
             resultSet.getInt("account_id"));
         }
-
     } catch(SQLException e){
       e.printStackTrace();
     }
     
     return customer;
   }
+
+  public static Account getAccount(int accountId) {
+    // use ? so that raw user input is not used; could be malicious/meant to steal or manip.
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+    try(Connection connection = connect();
+      PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setInt(1, accountId);
+
+      try (ResultSet resultSet = statement.executeQuery()) {
+        account = new Account(
+          resultSet.getInt("id"),
+          resultSet.getString("type"),
+          resultSet.getDouble("balance"));
+      }
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
+
+    return account;
+  }
   
   public static void main(String[] args){
     Customer customer = getCustomer("twest8o@friendfeed.com");
-    System.out.println(customer.getName());
+    Account account = getAccount(customer.getAccountId());
+    System.out.println(account.getBalance());
   }
 }
